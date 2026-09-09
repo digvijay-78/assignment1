2c8a251d40c75644755f736c0732bcd16bab407e
yash <yashr2896@gmail.com>
2026-09-09 16:53:33 +0530

    new

--- 7sep-recursion/10.py
+++ 7sep-recursion/10.py
@@ -8,17 +8,3 @@ Enter Coupon Number:
 29
 Output
 Prime Number"""
-
-def prime (n,i=2):
-    if n<2:
-        return 0
-    if i==n:
-         return 1
-    if n%i==0:
-            return 0
-    return prime(n,i+1)
-
-if prime(int(input("enter the no") ))==0:
-     print("not prime")
-else:
-     print("prime")
---
+++ 9sep/classwork.py
@@ -0,0 +1,58 @@
+# def add(a:int,b:int)->int:
+#     return a+b
+# print(add(10,20))
+
+# def add(a:int,b:int)->int:
+#     return "welcome"
+# print(add(10,20))
+
+# def add(a:int,b:int)->int:
+#     return a+b
+# print(add("wel","come"))#welcome
+
+# def add(a:int,b:int)->int:
+#     return a+b
+# print(add("wel","come"))#welcome
+
+# def add(a,b):
+#     """add two numbers and return results"""
+#     return a+b
+# print(add(10,20))
+
+# def add(a,b):
+#     """add two numbers and return results"""
+#     return a+b
+# print(add.__doc__)#add two numbers and return results
+
+
+
+# def add(a,b):
+#     """add two numbers and return results"""
+#     return a+b
+# print(len.__doc__)#Return the number of items in a container.
+# print(sum.__doc__)#When the iterable is empty, return the start value.
+# # This function is intended specifically for use with numeric values and may
+# # reject non-numeric types.
+
+# # print(help("modules"))
+
+# import math
+# print(math.sqrt(25))#5.0
+# print(math.ceil(4.2))#5
+# print(math.floor(4.9))#4
+# print(math.pi)#3.14
+# print(math.pow(2,5))#32.0
+# print(math.isfinite(25))#true
+# print(dir(math))
+# """['__doc__', '__loader__', '__name__', '__package__', '__spec__', 'acos', 'acosh', 'asin', 'asinh',
+# 'atan', 'atan2', 'atanh', 'cbrt', 'ceil', 'comb', 'copysign', 'cos', 'cosh', 'degrees', 'dist', 'e',
+# 'erf', 'erfc', 'exp', 'exp2', 'expm1', 'fabs', 'factorial', 'floor', 'fma', 'fmod', 'frexp', 'fsum', 
+# 'gamma', 'gcd', 'hypot', 'inf', 'isclose', 'isfinite', 'isinf', 'isnan', 'isqrt', 'lcm', 'ldexp', 'lgamma', 
+# 'log', 'log10', 'log1p', 'log2', 'modf', 'nan', 'nextafter', 'perm', 'pi', 'pow', 'prod', 'radians', 'remainder',
+# 'sin', 'sinh', 'sqrt', 'sumprod', 'tan', 'tanh', 'tau', 'trunc', 'ulp']"""
+
+
+# #diffence b/w pow and (**)
+# print(pow(2,3))
+# print(2**3)
+# print(2.0**3)
